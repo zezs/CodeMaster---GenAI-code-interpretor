4f3148e54fa23f96c67d86d2cbeb79a94270c04d
zezs <my.version.ctrl@gmail.com>
2024-08-02 23:51:19 +0200

    Implementing python and csv agents

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ from langchain import hub
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
 
 
 load_dotenv()
+
 
 def main():
     print("Start...")
 
     instructions = """You are an agent designed to write and execute python code to answer questions.
-        You have access to a python REPL, which you can use to execute python code.
-        If you get an error, debug your code and try again.
-        Only use the output of your code to answer the question. 
-        You might know the answer without running any code, but you should still run the code to get the answer.
-        If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
-        """
-
+    You have access to a python REPL, which you can use to execute python code.
+    If you get an error, debug your code and try again.
+    Only use the output of your code to answer the question. 
+    You might know the answer without running any code, but you should still run the code to get the answer.
+    If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
+    """
     base_prompt = hub.pull("langchain-ai/react-agent-template")
     prompt = base_prompt.partial(instructions=instructions)
 
-    # python repl executes python code in an interpretor(not recomended)
-    # uses our virtual env and its packages
-    # don't use in production
     tools = [PythonREPLTool()]
     agent = create_react_agent(
         prompt=prompt,
@@ -32,11 +30,25 @@ def main():
     )
 
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-
     agent_executor.invoke(
         input={
             "input": """generate and save in current working directory 15 QRcodes
                                 that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+        }
+    )
+
+    csv_agent = create_csv_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        path="episode_info.csv",
+        verbose=True,
+    )
+
+    csv_agent.invoke(
+        input={"input": "how many columns are there in file episode_info.csv"}
+    )
+    csv_agent.invoke(
+        input={
+            "input": "print the seasons by ascending order of the number of episodes they have"
         }
     )
 
